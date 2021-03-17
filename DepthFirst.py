20b63f810a425ead7a73e76101694af8ef8da53d
Gabriel Desroches <44992765+gabriel-desroches@users.noreply.github.com>
2021-03-17 12:37:28 -0400

    Renaming the Files
    Renaming because dashes break inporting

---
+++ DepthFirst.py
@@ -0,0 +1,3 @@
+
+def depthSearch(puzzle):
+    print ("hi")
--- Main.py
+++ Main.py
@@ -1,4 +1,9 @@
+from DepthFirst import *
+from IterativeDeepening import *
+from AStar import *
+
 print("Please enter a grid by entering numbers in the form: 1 2 3 4 5 6 7 8 9")
 print("| 1 2 3 |\n"
       "| 4 5 6 |\n"
       "| 7 8 9 |")
+
